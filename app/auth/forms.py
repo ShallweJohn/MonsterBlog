032d35f203bcd8a8b1278244cb3e736505680d64
ShallweJohn <jiangizhang@gmail.com>
2018-05-31 22:39:00 +0800

    完成登陆和注册界面交互

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import Form
 from wtforms import PasswordField, StringField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo, ValidationError
+
+from app.models import User
 
 
 class LoginForm(Form):
@@ -9,3 +11,24 @@ class LoginForm(Form):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log In')
+
+class RegistrationForm(Form):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64),
+                                             Email()])
+    # 通过使用WTForms提供的Regexp正则功能验证用户名格式
+    username = StringField('Username',validators=[
+        DataRequired(), Length(1, 64),Regexp('^[A-Za-z][A-Za-z0-9_.] *$', 0,
+                                            'Usernames must have only letters,'
+                                            'numbers, dots or underscores'
+                                            )])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo('password2', message='Passwrods must match.')])
+    password2 = PasswordField('Confirm password', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+    # 分别验证邮箱 和 用户名是否已存在
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('邮箱已被注册')
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('用户名已被使用')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,23 +1,48 @@
 from flask import redirect, url_for, render_template, session, current_app, request, flash
-from flask_login import login_user
+from flask_login import login_user, logout_user
+
+from app import db
 from app.auth import auth
-from app.auth.forms import LoginForm
+from app.auth.forms import LoginForm, RegistrationForm
 from app.models import User
 
 
 
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        flash('你现在可以登录')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html',form=form)
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
+    # 获取登陆表单对象
     form = LoginForm()
+    # 验证表单提交情况
     if form.validate_on_submit():
-        user = User.query.filter_by(email = form.email.data).first()
-
+        # 查询邮箱
+        user = User.query.filter_by(email=form.email.data).first()
+        # 验证用户名邮箱是否存在，验证密码
         if user is not None and user.verify_password(form.password.data):
-            login_user(user,  form.remember_me.data)
+            # 调用内建login_user函数，标记为已登陆用户
+            login_user(user, form.remember_me.data)
+            #
             return redirect(request.args.get('next')) or url_for('main.index')
-        flash('invalid username or password.')
+        # 电子邮件或密码不正确，发送flash消息，再次渲染表单，让用户重试登陆
+        flash('账户或密码不正确')
     return render_template('auth/login.html',form=form)
 
-        # next = request.args.get('next')
-        #return redirect(request.args.get('next') or url_for('main'))
+
+
+@auth.route('/logout')
+def logout():
+    logout_user()
+    flash('你已登出')
+    return redirect(url_for('main.index'))
+
